e5ec41ee1e065b1465bdb67b6b88bb86fa1ea2c9
don-spiro <nddugwuoke@gmail.com>
2023-11-16 12:47:21 -0800

    SIMPLE_SHELL

---
+++ built_ins.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * exec_env - a function that prints environment variables.
+ * @variables: variables whose value is to be retrived
+ *
+ * Return: 0 on success.
+*/
+
+void exec_env(char **variables)
+{
+	int i = 0;
+	char *value;
+
+	while (i < 12)
+	{
+		if (variables[i])
+		{
+			if (my_strncmp(variables[i], "games", 5) == 0)
+			{
+				value = getenv(variables[i]);
+
+				if (value)
+				{
+					my_fprintf(stdout, "%s:%s\n", variables[i], value);
+				}
+			}
+			else if (strcmp(variables[i], "_") == 0)
+			{
+				setenv(variables[i], "/bin/env", 1);
+			}
+			else
+			{
+				value = getenv(variables[i]);
+
+				if (value)
+				{
+					my_fprintf(stdout, "%s=%s\n", variables[i], value);
+				}
+			}
+			i++;
+		}
+		else
+			i++;
+	}
+}
